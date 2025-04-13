fbdc6782bc75242330d68aa7a4718cc516c2666a
daffadesra <desrad81@gmail.com>
2025-04-13 16:49:48 +0700

    add to cart functionality implemented

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import cart_view, add_to_cart, remove_from_cart
+from .views import cart_view, add_to_cart, remove_from_cart, update_quantity
 
 app_name = 'cart'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', cart_view, name='cart'),
     path('add/<str:product_id>/', add_to_cart, name='add_to_cart'),
     path('remove/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
+    path('update/<int:item_id>/', update_quantity, name='update_quantity'),
 ] 
